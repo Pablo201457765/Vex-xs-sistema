30f421f08ed1ccde60bf6f9a9b07ce3ee12ef22e
Pablo201457765 <p9112925@gmail.com>
2025-09-05 12:22:09 -0300

    Delete hora.js

--- hora.js
+++
@@ -1,9 +0,0 @@
-function actualizarHora() {
-    const lockTime = document.getElementById("lock-time");
-    const ahora = new Date();
-    const horas = ahora.getHours().toString().padStart(2,'0');
-    const minutos = ahora.getMinutes().toString().padStart(2,'0');
-    lockTime.textContent = `${horas}:${minutos}`;
-}
-setInterval(actualizarHora, 1000);
-actualizarHora();
